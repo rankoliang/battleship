74535c8dd6dc81363897a1db02b6c0cd5773d46e
Ranko Liang <rankoliang@gmail.com>
2021-03-04 15:49:16 -0500

    Fix bug when resetting in started phase
    The game would advance to the ended phase

--- src/features/game/gameHooks.js
+++ src/features/game/gameHooks.js
@@ -3,23 +3,29 @@ import { useDispatch, useSelector } from 'react-redux';
 import { selectPhase, gameReset, phaseAdvanced, winnerSet } from './gameSlice';
 import { selectOpponent } from '../players/playersSlice';
 import { useRemainingShips } from '../ships/shipHooks';
+import { selectShipsToBePlaced } from '../boards/boardsSlice';
 import { animateScroll as scroll } from 'react-scroll';
 
 export const useReset = () => {
   const dispatch = useDispatch();
 
   return () => {
-    debugger;
     dispatch(gameReset());
-    debugger;
   };
 };
 
 export const useWinnerDetermined = (player) => {
   const phase = useSelector(selectPhase);
   const shipsRemaining = useRemainingShips(player);
+  const shipsRemainingToBePlaced = useSelector((state) =>
+    selectShipsToBePlaced(state, player.boardId)
+  );
 
-  return phase === 'started' && shipsRemaining === 0;
+  return (
+    phase === 'started' &&
+    shipsRemaining === 0 &&
+    shipsRemainingToBePlaced === 0
+  );
 };
 
 export const useUpdateWinner = (player) => {
