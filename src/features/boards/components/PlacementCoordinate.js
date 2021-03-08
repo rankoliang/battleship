b0513b843270e93b408fa83006f7375766f257d4
Ranko Liang <rankoliang@gmail.com>
2021-03-08 18:29:13 -0500

    Replace invalid preview border with icon

--- src/features/boards/components/PlacementCoordinate.js
+++ src/features/boards/components/PlacementCoordinate.js
@@ -2,19 +2,13 @@ import { useContext } from 'react';
 import PlayerContext from '../../players/PlayerContext';
 import InteractableCoordinate from './InteractableCoordinate';
 import classNames from 'classnames';
+import { DefaultCoordinate } from './StyledCoordinates';
 
 const PlacementCoordinate = ({ coordinate, states }) => {
   const player = useContext(PlayerContext);
 
   if (player.computer) {
-    return (
-      <button
-        className={classNames('coordinate', {
-          coordinate__occupied: states.occupied,
-        })}
-        tabIndex="-1"
-      />
-    );
+    return <DefaultCoordinate tabIndex="-1" />;
   } else {
     return <InteractableCoordinate coordinateAPI={{ coordinate, states }} />;
   }
--- src/features/boards/components/PreviewableCoordinate.js
+++ src/features/boards/components/PreviewableCoordinate.js
@@ -1,28 +1,70 @@
 import { useContext } from 'react';
 import classNames from 'classnames';
 import CoordinateContext from '../contexts/CoordinateContext';
+import { ImBlocked } from 'react-icons/im';
 
-const PreviewableCoordinate = ({ ...props }) => {
+const PreviewableCoordinate = (props) => {
   const APIS = useContext(CoordinateContext);
   const { states } = APIS.coordinate;
   const { occupied, previewing } = states;
   const { isValidPlacement } = APIS.ship;
   const { isPreviewValid, setPreview, removePreview } = APIS.preview;
 
+  if (previewing) {
+    if (isPreviewValid) {
+      return (
+        <button
+          onFocus={setPreview}
+          onMouseEnter={setPreview}
+          onBlur={removePreview}
+          onMouseLeave={removePreview}
+          className={classNames('coordinate', 'coordinate__previewing', {
+            coordinate__occupied: occupied,
+            'coordinate--valid-placement': isValidPlacement,
+          })}
+          {...props}
+        />
+      );
+    } else {
+      return (
+        <InvalidPreviewPlacementCoordinate
+          onFocus={setPreview}
+          onMouseEnter={setPreview}
+          onBlur={removePreview}
+          onMouseLeave={removePreview}
+          className={classNames('coordinate', 'coordinate__previewing', {
+            coordinate__occupied: occupied,
+            'coordinate--valid-placement': isValidPlacement,
+          })}
+          {...props}
+        />
+      );
+    }
+  } else {
+    return (
+      <button
+        onFocus={setPreview}
+        onMouseEnter={setPreview}
+        onBlur={removePreview}
+        onMouseLeave={removePreview}
+        className={classNames('coordinate', {
+          coordinate__occupied: occupied,
+          'coordinate--valid-placement': isValidPlacement,
+        })}
+        {...props}
+      />
+    );
+  }
+};
+
+const InvalidPreviewPlacementCoordinate = ({ className, ...props }) => {
   return (
     <button
-      onFocus={setPreview}
-      onMouseEnter={setPreview}
-      onBlur={removePreview}
-      onMouseLeave={removePreview}
-      className={classNames('coordinate', {
-        coordinate__occupied: occupied,
-        coordinate__previewing: previewing,
-        'coordinate__previewing--invalid': previewing && !isPreviewValid,
-        'coordinate--valid-placement': isValidPlacement,
-      })}
+      className={classNames(className, 'coordinate__previewing--invalid')}
       {...props}
-    />
+    >
+      <ImBlocked />
+    </button>
   );
 };
 
