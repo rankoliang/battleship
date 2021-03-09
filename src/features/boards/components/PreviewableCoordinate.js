641508bc8577988f7777392bf085a11868a4a6f1
Ranko Liang <rankoliang@gmail.com>
2021-03-08 22:39:06 -0500

    Fix focus bug when tabbing during placement.
    Refactoring PreviewableCoordinate caused a bug where the active element
    would lose focus because the component was being replaced. The refactor
    has been reverted and the ImBlocked icon is added conditionally.

--- src/features/boards/components/PreviewableCoordinate.js
+++ src/features/boards/components/PreviewableCoordinate.js
@@ -3,62 +3,14 @@ import classNames from 'classnames';
 import CoordinateContext from '../contexts/CoordinateContext';
 import { ImBlocked } from 'react-icons/im';
 
-// Decides whether the coordinate goes into the preview state or not
-const PreviewableCoordinate = (props) => {
-  const APIS = useContext(CoordinateContext);
-  const { states } = APIS.coordinate;
-  const { previewing } = states;
-
-  if (previewing) {
-    return <PreviewingCoordinate {...props} />;
-  } else {
-    return <PreviewControllingCoordinate className="coordinate" {...props} />;
-  }
-};
-
-// A coordinate that has the coordinate__previewing style.
-// Two states: Blocked or Unblocked
-const PreviewingCoordinate = (props) => {
-  const APIS = useContext(CoordinateContext);
-  const { isPreviewValid } = APIS.preview;
-
-  if (isPreviewValid) {
-    return (
-      <PreviewControllingCoordinate
-        className="coordinate coordinate__previewing"
-        {...props}
-      />
-    );
-  } else {
-    return (
-      <BlockedPreviewingCoordinate
-        className="coordinate coordinate__previewing"
-        {...props}
-      />
-    );
-  }
-};
-
-// Shows a red cross icon in the center
-const BlockedPreviewingCoordinate = ({ className, ...props }) => {
-  return (
-    <PreviewControllingCoordinate
-      className={classNames(className, 'coordinate__previewing--invalid')}
-      {...props}
-    >
-      <ImBlocked />
-    </PreviewControllingCoordinate>
-  );
-};
-
 // A Coordinate that controls the preview state based on mouse movements
 // or toggling focus
-const PreviewControllingCoordinate = ({ className, ...props }) => {
+const PreviewableCoordinate = ({ className, ...props }) => {
   const APIS = useContext(CoordinateContext);
   const { states } = APIS.coordinate;
-  const { occupied } = states;
+  const { occupied, previewing } = states;
   const { isValidPlacement } = APIS.ship;
-  const { setPreview, removePreview } = APIS.preview;
+  const { isPreviewValid, setPreview, removePreview } = APIS.preview;
 
   return (
     <button
@@ -66,12 +18,16 @@ const PreviewControllingCoordinate = ({ className, ...props }) => {
       onMouseEnter={setPreview}
       onBlur={removePreview}
       onMouseLeave={removePreview}
-      className={classNames(className, {
+      className={classNames('coordinate', {
         coordinate__occupied: occupied,
+        coordinate__previewing: previewing,
+        'coordinate__previewing--invalid': previewing && !isPreviewValid,
         'coordinate--valid-placement': isValidPlacement,
       })}
       {...props}
-    ></button>
+    >
+      {previewing && !isPreviewValid && <ImBlocked />}
+    </button>
   );
 };
 
